114afc76492b71971c23a33838834d295a2f9ed6
DheenodaraRao <dheenodararao@gmail.com>
2018-05-16 15:55:03 +0800

    Added ShowSocial COmponent

---
+++ socialcard/src/Components/ShowSocial.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class ShowSocial extends Component{
+    render(){
+        return(
+            <div id="showSocial">
+                <p>
+                    Hi Social Card.  
+                </p>
+            </div>
+        );
+    }
+}
